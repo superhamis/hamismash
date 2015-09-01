0ac3c0549618ee1e6e2d2878c9aaae137036c9e9
Mikko Kumara <mikko.kumara@gmail.com>
2015-09-01 20:27:09 +0300

    more code

--- Hamismash/Assets/src/PlayerMovement.cs
+++ Hamismash/Assets/src/PlayerMovement.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour {
 
 	public float speed = 0.5f;
 
---
+++ Hamismash/Assets/src/SpawnBuildings.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnBuildings : MonoBehaviour {
+
+	public GameObject buildingPrefab;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i<10; i++) {
+			Instantiate(buildingPrefab, new Vector3(Random.Range(-20,20), 0, Random.Range(-20,20)), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (Input.GetKey(KeyCode.Space)) {
+//			gameObject.AddComponent<SpawnBuildings>();
+			//Instantiate(buildingPrefab, new Vector3(Random.Range(-20,20), 0, Random.Range(-20,20)), Quaternion.identity);
+		//}
+	}
+}
