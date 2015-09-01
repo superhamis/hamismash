208398550e134875d008829727491e05b463c6c9
Mikko Kumara <mikko.kumara@gmail.com>
2015-09-01 19:31:32 +0300

    the basic game files

---
+++ Hamismash/Assets/src/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W)) {
+			transform.Translate(speed*Vector3.forward);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			transform.Translate(speed*Vector3.left);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			transform.Translate(speed*Vector3.back);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			transform.Translate(speed*Vector3.right);
+		}
+	}
+}
